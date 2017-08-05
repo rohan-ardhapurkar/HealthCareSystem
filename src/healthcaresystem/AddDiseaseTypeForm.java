69ad84442fd9e24321b0b8189bdd7672468aabf7
Sandip <Sandip@Creazione-PC3>
2017-08-05 18:07:44 +0530

    new forms added

--- src/healthcaresystem/AddDiseaseTypeForm.java
+++ src/healthcaresystem/AddDiseaseTypeForm.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package healthcaresystem;
 
 import javax.swing.JButton;
@@ -10,29 +5,31 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class AddDiseaseTypeForm extends JPanel{
+public class AddDiseaseTypeForm extends JPanel {
+
     JLabel lbldtypename;
     JTextField txtdtypename;
     JButton btnAdddtype;
-    AddDiseaseTypeForm(){
+
+    AddDiseaseTypeForm() {
         setLayout(null);
-        
-        lbldtypename=new JLabel("ADD DISEASE TYPE HERE");
-        
-        lbldtypename.setBounds(70,150,150,30);
-        
-        txtdtypename=new JTextField();
-        
-        txtdtypename.setBounds(250,150,250,30);
-        
-        btnAdddtype=new JButton("ADD DISEASE TYPE");
-        
-        btnAdddtype.setBounds(250,250,250,30);
+        //create label
+        lbldtypename = new JLabel("ADD DISEASE TYPE HERE");
+        //set label position
+        lbldtypename.setBounds(70, 150, 150, 30);
+        //create textbox 
+        txtdtypename = new JTextField();
+        //set textbox position
+        txtdtypename.setBounds(250, 150, 250, 30);
+        //create buttons
+        btnAdddtype = new JButton("ADD DISEASE TYPE");
+        //set button positions 
+        btnAdddtype.setBounds(250, 250, 250, 30);
         add(lbldtypename);
         add(txtdtypename);
         add(btnAdddtype);
         setVisible(true);
-        setSize(800,800);
-        
+        setSize(800, 800);
+
     }
 }
